165c6e6480e443815fb6d63d85f00ea56525b852
David Torralba Goitia <david.torralba.goitia@gmail.com>
2020-12-05 14:46:11 +0000

    Puzzle 10

---
+++ puzzles/puzzle_10/logic.py
@@ -0,0 +1,53 @@
+from itertools import zip_longest
+from pathlib import Path
+from typing import Any, Iterator, List
+
+
+def get_boarding_passes(path: Path) -> Iterator[str]:
+    with path.open("r") as f:
+        for line in f:
+            yield line.strip()
+
+
+def resolve(input_paths: List[Path]) -> Any:
+    boarding_pass_input_path = input_paths[0]
+
+    seat_ids: List[int] = []
+    for boarding_pass in get_boarding_passes(boarding_pass_input_path):
+        row_code, column_code = boarding_pass[:7], boarding_pass[7:]
+
+        row_range = [0, 127]
+        column_range = [0, 7]
+        for i, (row_char, column_char) in enumerate(zip_longest(row_code, column_code)):
+            row_span = 2 ** (7 - i - 1)
+            if row_char == "F":
+                row_range[1] -= row_span
+            else:  # char == "B"
+                row_range[0] += row_span
+
+            if not column_char:
+                continue
+
+            column_span = 2 ** (3 - i - 1)
+            if column_char == "L":
+                column_range[1] -= column_span
+            else:  # char == "B"
+                column_range[0] += column_span
+
+        row = row_range[0]
+        column = column_range[0]
+
+        seat_id = int(row * 8 + column)
+        seat_ids.append(seat_id)
+
+    sorted_seat_ids = iter(sorted(seat_ids))
+    prev_seat_id = next(sorted_seat_ids)
+    for next_seat_id in sorted_seat_ids:
+        if prev_seat_id + 1 == next_seat_id:
+            prev_seat_id = next_seat_id
+        else:
+            break
+
+    seat_id = next_seat_id - 1
+
+    return seat_id
