8a4d3e2ec7a2f0bde992773b9869f5e72d0b1ada
David Torralba Goitia <david.torralba.goitia@gmail.com>
2020-12-06 10:12:34 +0000

    Puzzle 11

---
+++ puzzles/puzzle_11/logic.py
@@ -0,0 +1,27 @@
+from itertools import zip_longest
+from pathlib import Path
+from typing import Any, Iterator, List
+
+
+def get_forms(path: Path) -> Iterator[str]:
+    form_lines = []
+    with path.open("r") as f:
+        for line in f:
+            if line == "\n":
+                yield "".join(form_lines)
+                form_lines = []
+            else:
+                form_lines.append(line.strip())
+        else:
+            yield "".join(form_lines)
+
+
+def resolve(input_paths: List[Path]) -> Any:
+    forms_input_path = input_paths[0]
+
+    answer_amount_per_group = []
+    for form in get_forms(forms_input_path):
+        answers = {answer for answer in form}
+        answer_amount_per_group.append(len(answers))
+
+    return sum(answer_amount_per_group)
---
+++ puzzles/puzzle_11/test_logic.py
@@ -0,0 +1,37 @@
+from pathlib import PosixPath
+from textwrap import dedent
+
+
+from .logic import resolve
+
+
+def test_resolve(tmp_path: PosixPath) -> None:
+    # 1 person per line
+    # groups split by empty line
+
+    forms_input_path = tmp_path / "input_1_forms"
+    forms_input_path.write_text(
+        dedent(
+            """
+            abc
+
+            a
+            b
+            c
+
+            ab
+            ac
+
+            a
+            a
+            a
+            a
+
+            b
+            """
+        ).strip()
+    )
+
+    result = resolve(input_paths=[forms_input_path])
+
+    assert result == 11
